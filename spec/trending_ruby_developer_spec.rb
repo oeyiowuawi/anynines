3884c64adb42fc6017510d9c8a15065ec479a2c2
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2019-12-10 13:13:00 +0100

    add webmock to disable web connection in test mode

---
+++ spec/trending_ruby_developer_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe ' get all the trending ruby developers on github' do
+  it 'returns a status of 200' do
+    get '/trending'
+
+    expect(response).to have_http_status(:success)
+  end
+end
