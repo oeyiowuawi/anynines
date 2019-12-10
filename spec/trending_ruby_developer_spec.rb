4ec932ae81a2528f0ca013539cd731be96fceb84
Olalekan Eyiowuawi <olalekan.eyiowuawi@andela.com>
2019-12-10 13:39:07 +0100

    add spec for retrieving the developers

--- spec/trending_ruby_developer_spec.rb
+++ spec/trending_ruby_developer_spec.rb
@@ -6,4 +6,18 @@ describe ' get all the trending ruby developers on github', type: :request do
 
     expect(response).to have_http_status(:success)
   end
+
+  it 'returns a json' do
+    stub_github_request
+    get '/trending'
+
+    expect(JSON.parse(response.body).first.username).to eq('dhh')
+  end
+
+
+  def stub_github_request
+    stub_request(:get, /github-trending-api.now/).
+      with(headers: {'Accept'=>'*/*', 'User-Agent'=>'Ruby'}).
+      to_return(status: 200, body: "stubbed response", headers: {})
+  end
 end
